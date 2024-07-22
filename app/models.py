473d54ed452e2334b9db47c04b5a5cb90372fdcd
MarinaEduarda <161317262+MarinaEduarda@users.noreply.github.com>
2024-07-22 09:52:00 -0300

    Add files via upload

--- app/models.py
+++ app/models.py
@@ -1,3 +1,97 @@
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Tipos de produtos")
+    def __str__(self):
+        return f"{self.nome}"
+class Meta:
+    verbose_name = "Produto"
+    verbose_name_plural = "Produtos"
+
+class Estoque(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, verbose_name="Nome do produto")
+    imagem = models.ImageField(verbose_name="imagem")
+    medida = models.DecimalField(max_digits=8, decimal_places=2, verbose_name="medida")
+    unid_medida = models.CharField(max_length=2, verbose_name="unidade de medida")
+    lote = models.IntegerField(verbose_name="lote")
+    data_fabricacao = models.DateField(verbose_name="data de fabricação")
+    data_validade = models.DateField(verbose_name="data de validade")
+    def __str__(self):
+        return f"{self.produto}, {self.imagem}, {self.medida}, {self.unid_medida}, {self.lote}, {self.data_fabricacao}, {self.data_validade}"
+
+class Meta:
+    verbose_name = "Estoque"
+    verbose_name_plural = "Estoques"
+
+class Setor(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Nome do setor")
+    def __str__(self):
+        return f"{self.nome}"
+
+class Meta:
+    verbose_name = "Setor"
+    verbose_name_plural = "Setores"
+
+class Animal(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Animais trabalhados")
+    setor = models.ForeignKey(Setor,on_delete=models.CASCADE, verbose_name="Setor pertencente")
+
+    def __str__(self):
+        return f"{self.nome}, {self.setor}"
+
+class Meta:
+    verbose_name = "Animal"
+    verbose_name_plural = "Animais"
+
+class Funcao(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Função")
+
+    def __str__(self):
+        return f"{self.nome}"
+
+class Meta:
+    verbose_name = "Funcao"
+    verbose_name_plural = "Funcoes"
+
+class Autorizado(models.Model):
+    nome = models.CharField(max_length=100, verbose_name="Nome")
+    CPF = models.IntegerField(verbose_name = "CPF")
+    email = models.CharField(max_length=100, verbose_name="email")
+    funcao = models.ForeignKey(Funcao, on_delete=models.CASCADE, verbose_name="Funcao")
+    setor = models.ForeignKey(Setor, on_delete=models.CASCADE, verbose_name="Funcao")
+
+    def __str__(self):
+        return f"{self.nome}, {self.CPF}, {self.email}, {self.funcao}, {self.setor}"
+
+class Meta:
+    verbose_name = "Autorizado"
+    verbose_name_plural = "Autorizados"
+
+class Saida(models.Model):
+    autorizado = models.ForeignKey(Setor,on_delete=models.CASCADE, verbose_name="Pessoa autorizada")
+    data = models.DateField(verbose_name ="data da saida")
+    horario = models.TimeField(verbose_name = "horario da saida")
+
+    def __str__(self):
+        return f"{self.autorizado}, {self.data}, {self.horario}"
+
+class Meta:
+    verbose_name = "Saida"
+    verbose_name_plural = "Saidas"
+
+class Detalhes(models.Model):
+    saida = models.ForeignKey(Saida,on_delete=models.CASCADE, verbose_name="Saída referente")
+    estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE, verbose_name="Produto retirado")
+    quantidade = models.IntegerField(verbose_name = "quantidade")
+    unid_medida = models.CharField(max_length=2, verbose_name="unidade de medida")
+
+    def __str__(self):
+        return f"{self.saida}, {self.estoque}, {self.quantidade}, {self.unid_medida}"
+
+class Meta:
+    verbose_name = "Detalhes"
+    verbose_name_plural = "Detalhes"
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8t0=3d#*f*ul(d$#8w0&9v%h(_x67u!n!g@xri1qyz!ynz7&49'
+SECRET_KEY = 'django-insecure-y!zji@#pp1k5-u(g=$+t+y3_&*3jj-0aihtr86hc6nj!rfl5_l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-import os 
 
 # Application definition
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'app/templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'app/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,11 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(STATIC_URL, 'static')
-
+STATIC_ROOT = os.path.join(STATIC_URL, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app/static/')
+os.path.join(BASE_DIR, "app/static/"),
 ]
 
 # Default primary key field type
